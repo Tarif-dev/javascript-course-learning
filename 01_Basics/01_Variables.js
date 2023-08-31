ebc391cf286ad70dd16e78db644369883b86b6d1
Tarif <143481028+Tarif-dev@users.noreply.github.com>
2023-08-31 02:18:48 +0000

    Variables and Datatypes Done

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,18 @@
+const constantvariable = 123465
+let variable1 = "ahkjdh"
+var variable2 = "alkdsfj"
+variable3 = 213215
+let variable4;
+
+variable1 = "shdfu"
+variable2 = "adkshs"
+variable3 = 465465
+
+console.log(constantvariable)
+console.table([variable1, variable2, variable3, constantvariable, variable4])
+
+
+/*
+Never use var to declare variable
+because of issues in block scope and functional scope.
+*/
---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,1 @@
+"use strict" // this ensures that the entire code is treated as the latest version of JS
